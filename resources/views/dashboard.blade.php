d69073bc359f5692fec85c0cdb0c7139c633f6a4
ReinKaos95 <malcom2209@hotmail.com>
2021-03-22 13:00:57 -0400

    actualizacion

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,6 +5,10 @@
         </h2>
     </x-slot>
 
+        <!-- Fonts -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
