0cf9630c3f6f357f33ec63518601f33eb8609158
ReinKaos95 <malcom2209@hotmail.com>
2021-04-05 12:35:22 -0400

    controllers

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,9 +16,9 @@
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
-                        <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 underline">Dashboard</a>
+                        <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 underline">{{ Auth::user()->name }}</a>
                     @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
+                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Registrarse</a>
                     @endauth
                 </div>
             @endif
