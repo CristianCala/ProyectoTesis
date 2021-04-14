6a6e6db5cda0313c435d5cb95caade861faf498b
ReinKaos95 <malcom2209@hotmail.com>
2021-04-14 12:16:36 -0400

    sedders

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -4,26 +4,19 @@
 <table class="table-fixed">
   <thead>
     <tr>
-      <th class="w-1/2 ...">Title</th>
-      <th class="w-1/4 ...">Author</th>
-      <th class="w-1/4 ...">Views</th>
+      <th class="w-1/2 ...">Nombre</th>
+      <th class="w-1/4 ...">Email</th>
+
     </tr>
   </thead>
   <tbody>
     <tr>
-      <td>Intro to CSS</td>
-      <td>Adam</td>
-      <td>858</td>
+    @if($users)
+        @foreach($users as $key)
+      <td>{{$key->name}}</td>
+      <td>{{$key->email}}</td>
     </tr>
-    <tr class="bg-blue-200">
-      <td>A Long and Winding Tour of the History of UI Frameworks and Tools and the Impact on Design</td>
-      <td>Adam</td>
-      <td>112</td>
-    </tr>
-    <tr>
-      <td>Intro to JavaScript</td>
-      <td>Chris</td>
-      <td>1,280</td>
-    </tr>
+    @endforeach
+        @endif
   </tbody>
 </table>
