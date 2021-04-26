bd714f93f58b9372ff4a6f39a7efa1d20f8e1f52
ReinKaos95 <malcom2209@hotmail.com>
2021-04-25 23:44:23 -0400

    ventanas modales

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,13 +1,17 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Ventana hecha con una plantilla Blade</title>
-	 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <title>Ventana hecha con una plantilla Blade</title>
+   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
 <center>
-	<h1 class="bg-gray-200">Configuracion de Usuarios</h1>
+  <h1 class="bg-gray-200">Configuracion de Usuarios</h1>
 <p class="mb-2 mt-2">Usuarios listados</p>
 
 <div class="container mt-6">
@@ -26,14 +30,14 @@
         @foreach($users as $key)
       <td class="border border-gray-400 px-4 py-2 text-gray-800">{{$key->name}}</td>
       <td class="border border-gray-400 px-4 py-2 text-gray-800">{{$key->email}}</td>
-      <td class="border border-gray-400 px-4 py-2 text-gray-800"><button class="mb-4 bg-yellow-200 rounded-md">boton</button> <button class="mb-4 bg-blue-400 rounded-md">boton</button></td>
+      <td class="border border-gray-400 px-4 py-2 text-gray-800">@include('admin.forms.editUser')  @include('admin.vistasUsuario.verUsuario')</td>
     </tr>
     @endforeach
     @endif
   </tbody>
 </table>
 <div class="mt-4">
-	      {{$users->links()}}
+        {{$users->links()}}
   <a href="{{url('/dashboard')}}" class="btn bg-gray-100 rounded-md text-2xl">Volver</a>
 </div>
 </div>
