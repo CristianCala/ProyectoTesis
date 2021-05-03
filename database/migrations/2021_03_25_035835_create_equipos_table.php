319a6a4f08ac91a7ce76b2dda3705be863b3d60b
ReinKaos95 <malcom2209@hotmail.com>
2021-05-03 15:35:09 -0400

    Validacion de datos

---
+++ database/migrations/2021_03_25_035835_create_equipos_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEquiposTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('equipos', function (Blueprint $table) {
+          $table->id();
+            $table->string('eq_modelo');
+            $table->string('eq_marca');
+            $table->string('eq_serial');
+            //$table->integer('eq_tequid')->references('id')->on('tipo_equipos');
+            $table->string('eq_nbiennacional');
+            $table->string('eq_estatus');
+            //$table->integer('departamentos_dep_id')->references('id')->on('departamentos');
+            //$table->integer('ubicacion_equipo_ubeq')->references('ubeq_id')->on('ubicacion_equipos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('equipos');
+    }
+}
