cf54ef644f6f95d86a5ac86380896fd95d870713
ReinKaos95 <malcom2209@hotmail.com>
2021-07-04 00:41:20 -0430

    Ventanas de configuracion

--- resources/views/config/index.blade.php
+++ resources/views/config/index.blade.php
@@ -11,9 +11,9 @@
   </div>
   <div class="tab-content" id="v-pills-tabContent">
     <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">@include('config.opciones.rols')</div>
-    <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">...</div>
-    <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">...</div>
-    <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">...</div>
+    <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">@include('config.opciones.perfil')</div>
+    <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">@include('config.opciones.aith')</div>
+    <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">@include('config.opciones.respaldo')</div>
   </div>
 </div>
 
