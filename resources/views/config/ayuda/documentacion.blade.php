7b4a1cf2e9c37c1f8e08e24512a8eb90f1f7bac7
ReinKaos95 <malcom2209@hotmail.com>
2021-07-17 21:25:41 -0430

    ventanas de ayuda

---
+++ resources/views/config/ayuda/documentacion.blade.php
@@ -0,0 +1,10 @@
+<div class="container">
+  <h1>Documentacion</h1>
+  <h5>Este proyecto data sobre un sistema de gestion e inventario para la gerencia de tecnologia e informacion.</h5>
+  <h5 El sistema procese el registro de cada producto ingresado con las siguientes caracteristicas:</h5>
+<p>Antes de entrar al sistema se requiere un usuario, cada usuario debe poseer un rol.</p>
+<p>Opciones por rol</p>
+<p>Rutas protegidas</p>
+<p>Procesamiento de archivos y ordenes.</p>
+
+</div>
